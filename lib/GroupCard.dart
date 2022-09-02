d28c2e136701d609016c2382794bd6155d803b39
Quentin 'Torvald' LAURENT <ql2697@gmail.com>
2022-09-02 19:06:05 +0200

    :sparkles: Started homepage design implementation

---
+++ lib/GroupCard.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class GroupCard extends StatelessWidget {
+  String title;
+  String description;
+  double price;
+
+  GroupCard(
+      {Key? key,
+      required this.title,
+      required this.price,
+      required this.description})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white,
+      elevation: 2.0,
+      child: Container(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          verticalDirection: VerticalDirection.down,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const Padding(padding: EdgeInsets.only(bottom: 6.0)),
+            Text(description, style: const TextStyle(fontSize: 14)),
+            const Padding(padding: EdgeInsets.only(bottom: 6.0)),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('${price.toStringAsFixed(2)}€'),
+                Text(
+                  '${price.toStringAsFixed(2)}€',
+                  style: const TextStyle(fontSize: 14),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
